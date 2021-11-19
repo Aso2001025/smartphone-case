c15bfc3eb8fe0509ba3fcbf777d47ace497ad3c1
Aso2001017 <83050700+Aso2001017@users.noreply.github.com>
2021-11-19 11:29:12 +0900

    Add files via upload

---
+++ ソースコード/toppage/js/header.js
@@ -0,0 +1,38 @@
+document.write(
+    '<div id="relative"></div>'+
+    '<h1 class="title"><a href="">タイトル</a></h1>' +
+    '\n' +
+    '<!-- ヘルプページリンク -->\n' +
+    '<div class="help"><a href=""><img src="img/？button.png"\n' +
+    'alt="help_image"></a></div>\n' +
+    '\n' +
+    '    <div class="hamburger-menu">\n' +
+    '        <input type="checkbox" id="menu-btn-check">\n' +
+    '        <label for="menu-btn-check" class="menu-btn"><span></span></label>\n' +
+    '        <!--ここからメニュー-->\n' +
+    '        <div class="menu-content">\n' +
+    '            <ul>\n' +
+    '                <li>\n' +
+    '                    <a href="#">ログイン</a>\n' +
+    '                </li>\n' +
+    '                <li>\n' +
+    '                    <a href="#">ログアウト</a>\n' +
+    '                </li>\n' +
+    '                <li>\n' +
+    '                    <a href="#">会員情報</a>\n' +
+    '                </li>\n' +
+    '                <li>\n' +
+    '                    <a href="#">保存デザイン一覧</a>\n' +
+    '                </li>\n' +
+    '                <li>\n' +
+    '                    <a href="#">デザインを探す</a>\n' +
+    '                </li>\n' +
+    '                <li>\n' +
+    '                    <a href="#">カート</a>\n' +
+    '                </li>\n' +
+    '                <li>\n' +
+    '                    <a href="#">注文履歴</a>\n' +
+    '                </li>\n' +
+    '            </ul>\n' +
+    '        </div>\n' +
+    '    </div>\n');
